9bde808fe6183fc9f7eb630eb70277b5eb416152
uksus14 <akylov7adil92@gmail.com>
2024-08-03 07:02:12 +0600

    JJK manga change the url and made it send pages directly to the chat

--- JJK_manga/jjk_manga.py
+++ JJK_manga/jjk_manga.py
@@ -3,16 +3,18 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from time import sleep
 from telebot import TeleBot
+from telebot import types
 from dotenv import load_dotenv
+from itertools import batched
 import os
 
-URL = "https://ww2.jujustukaisen.com/"
+URL = "https://w7.jujustu-kaisen.com/"
 TIME_BETWEEN_CHECKS = timedelta(minutes=30)
 SECONDARY_TIME = timedelta(minutes=5)
 
 load_dotenv()
 TOKEN = os.getenv("token")
-CHAT_ID = int(os.getenv("chat-id"))
+CHAT_ID = os.getenv("chat-id")
 
 bot = TeleBot(TOKEN)
 
@@ -21,8 +23,16 @@ def get_latest():
     print(f"{datetime.now().strftime("%H:%M").rjust(5)}: Request sent")
     return BeautifulSoup(site.text, "html.parser").find("li", {"id": "ceo_latest_comics_widget-3"}).find("ul").find("li").find("a")["href"]
 
-def send(text):
-    bot.send_message(CHAT_ID, text)
+def send(url):
+    site = requests.get(url)
+    soup = BeautifulSoup(site.text, "html.parser")
+    content = soup.find("div", {"class": "entry-content"})
+    parags = content.find_all("p")
+    pages = [parag.find("img")["data-src"] for parag in parags]
+    files = [types.InputMediaPhoto(page, has_spoiler=True) for page in pages]
+    bot.send_message(CHAT_ID, f"[New Chapter!]({url})", parse_mode='MARKDOWN')
+    for group in batched(files, 10):
+        bot.send_media_group(CHAT_ID, group)
 
 def wait4check():
     while last_check + TIME_BETWEEN_CHECKS > datetime.now():
@@ -40,5 +50,5 @@ while True:
     last_check = datetime.now()
     if latest != new:
         print(f"New chapter detected! ({new})")
-        latest = new
         send(latest)
+        latest = new
